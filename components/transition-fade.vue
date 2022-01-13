6ba6fccbd78ffa1b46cb2f7e9ff99996edb7f0b5
Łukasz Florczak <lukasz@florczak.dev>
2022-01-13 09:54:59 +0100

    Add transition and hydration problem demo

---
+++ components/transition-fade.vue
@@ -0,0 +1,18 @@
+<template>
+  <transition name="fade">
+    <slot/>
+  </transition>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+  .fade-enter-from, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.3s;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <h1>Hello World!</h1>
+    <button @click="show = !show">Toggle text</button>
+
+    <transition-fade>
+      <h1 v-if="show">Hello World!</h1>
+    </transition-fade>
   </div>
 </template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import TransitionFade from '../components/transition-fade.vue'
+
+  const show = ref(false)
+</script>
