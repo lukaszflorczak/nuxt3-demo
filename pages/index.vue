9a16118b9a293ccd50047fe635497ec313ffc8c8
Łukasz Florczak <lukasz@florczak.dev>
2022-02-10 14:59:04 +0100

    Add `definePageMeta`

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <p>Run the <code>/</code> page. Next go to <code>app.vue</code> and remove <code>nuxt-layout</code> and refresh the page</p>
+    <p>Run the <code>/</code> page. Next change <code>layout</code> value in <code>index.vue</code> to <code>false</code></p>
 
     <timestamp/>
   </div>
 </template>
 
 <script lang="ts" setup>
+  import { definePageMeta } from '#imports'
+
   import Timestamp from '~/components/timestamp.vue'
+
+  definePageMeta({
+    layout: 'default' // Change it to `false`
+  })
 </script>
