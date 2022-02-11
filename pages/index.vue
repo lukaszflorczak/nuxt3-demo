49d0d68cdd996ca15a93a2a6bf28b39f2970b95f
Łukasz Florczak <lukasz@florczak.dev>
2022-02-11 10:58:52 +0100

    Add scroll behavior demo

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
     <h1>Hello World!</h1>
+
+    <nav>
+      <a v-for="n in 5" :href="`#section-${n}`" style="margin-right: 30px;">Go to {{ n }}</a>
+    </nav>
+
+    <section v-for="n in 5" :id="`section-${n}`" style="height: 100vh;">
+      <h1>Section {{ n }}</h1>
+    </section>
+
   </div>
 </template>
